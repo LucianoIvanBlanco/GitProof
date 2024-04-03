e7dd450bd36a33cfbb50fc8fe5d8886988f51aa7
Luciano Blanco <122919958+LucianoIvanBlanco@users.noreply.github.com>
2024-04-03 10:56:36 +0200

    master: fourth commit

--- GitProof.java
+++ GitProof.java
@@ -2,5 +2,6 @@ public class GitProof {
 public static void main(String[] args) {
     System.out.println("Empleado: Luciano Blanco");
     System.out.println("Modificacion desde master");
+    System.out.println("Segunda odificacion desde master");
 }
 }
