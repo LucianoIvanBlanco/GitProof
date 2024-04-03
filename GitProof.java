6b22fb2ae96e57247117a38fd5fd1e03d5fc514a
Luciano Blanco <122919958+LucianoIvanBlanco@users.noreply.github.com>
2024-04-03 12:23:51 +0200

    feat: second commit second name

--- GitProof.java
+++ GitProof.java
@@ -1,11 +1,11 @@
 public class GitProof {
 public static void main(String[] args) {
-    System.out.println("Empleado: Luciano Blanco");
+    
     System.out.println("Modificacion desde master");
 
     System.out.println("Segunda odificacion desde master");
 
-    System.out.println("Modificacion desde feature");
+    System.out.println("Modificacion desde feature en rama 1");
 
 }
 }
